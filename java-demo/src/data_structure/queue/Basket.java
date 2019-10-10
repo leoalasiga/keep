2a1bc31f8e86d7205c230598f3890366331b514b
leoalasiga <leoalasiga@icloud.com>
2019-10-10 15:35:38 +0800

    the first java demo of Hello world

---
+++ java-demo/src/data_structure/queue/Basket.java
@@ -0,0 +1,11 @@
+package data_structure.queue;
+
+/**
+ * @ClassName Basket
+ * @Description TODO
+ * @Author 刘嘉杰
+ * @Date 2019/10/10 14:52
+ * @Version 1.0
+ */
+public class Basket {
+}
---
+++ java-demo/src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+/**
+ * @ClassName HelloWorld
+ * @Description first java demo
+ * @Author 刘嘉杰
+ * @Date 2019/10/10 14:55
+ * @Version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
