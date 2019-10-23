f2ecfb2354d5f3abfc22dec22d6a4b5d3dc9d05f
leoalasiga <leoalasiga@icloud.com>
2019-10-23 16:53:06 +0800

    添加栈ç的demo

--- java-demo/src/common/test/TestDemo.java
+++ java-demo/src/common/test/TestDemo.java
@@ -1,11 +1,21 @@
 package common.test;
 
-import java.util.stream.Stream;
-
 public class TestDemo {
     public static void main(String[] args) {
-        String[] a = new String[10];
-        Stream.of(a).forEach(s-> System.out.println(s));
-        System.out.println(a.length);
+        Integer i = 100;
+        Integer b = 100;
+        System.out.println(i==b);
+        Integer c = 127;
+        Integer d = 127;
+        System.out.println(c==d);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    protected void finalized() {
+
     }
 }
---
+++ java-demo/src/data_structure/stack/StackDemo.java
@@ -0,0 +1,11 @@
+package data_structure.stack;
+
+/**
+ * @ClassName StackDemo
+ * @Description TODO
+ * @Author 刘嘉杰
+ * @Date 2019/10/23 15:56
+ * @Version 1.0
+ */
+public class StackDemo {
+}
