715f3d3b82f6d3ec4de3841c00f739e9b78fc5b0
leoalasiga <leoalasiga@icloud.com>
2019-10-23 16:54:26 +0800

    添加Stack的demo

--- java-demo/src/data_structure/stack/StackDemo.java
+++ java-demo/src/data_structure/stack/StackDemo.java
@@ -1,5 +1,7 @@
 package data_structure.stack;
 
+import java.util.Stack;
+
 /**
  * @ClassName StackDemo
  * @Description TODO
@@ -8,4 +10,39 @@ package data_structure.stack;
  * @Version 1.0
  */
 public class StackDemo {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        System.out.println("stack:" + stack);
+        showPush(stack, 22);
+        showPush(stack, 44);
+        showPush(stack, 66);
+        showPush(stack, 88);
+
+        showPop(stack);
+        showPop(stack);
+        showPop(stack);
+        showPop(stack);
+
+        try {
+            showPop(stack);
+        } catch (Exception e) {
+            System.out.println("empty stack");
+        }
+
+    }
+
+    static void showPush(Stack<Integer> stack, int a) {
+        stack.push(new Integer(a));
+        System.out.println("push (" + a + ")");
+        System.out.println("stack: " + stack);
+    }
+
+
+    static void showPop(Stack<Integer> stack) {
+        Integer pop = stack.pop();
+        System.out.println("pop->" + pop);
+        System.out.println("stack: " + stack);
+
+
+    }
 }
